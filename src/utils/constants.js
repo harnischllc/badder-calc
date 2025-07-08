96eb9b9259a109d802f3a9ac3d9e6dc99663a324
harnischllc <harnischllc@gmail.com>
2025-07-08 14:12:38 -0500

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-// Default MLB data - will be overridden by GitHub data when available
+// Default MLB data - based on 2024 market rates
 export const LEAGUE_DATA = {
   avgWAR: 2.0,
   avgSalary: 4500000,
@@ -11,27 +11,18 @@ export const LEAGUE_DATA = {
   }
 };
 
-// Remove PRESET_PLAYERS since we'll use real data
-export const PRESET_PLAYERS = [];
-
-// $/WAR thresholds for categorization
+// $/WAR thresholds for categorization (in millions)
 export const WAR_VALUE_THRESHOLDS = {
-  historicBargain: 1.0,  // < $1M per WAR
-  highValue: 4.0,        // < $4M per WAR
-  average: 10.0,         // < $10M per WAR
+  historicBargain: 1.0,   // < $1M per WAR
+  highValue: 4.0,         // < $4M per WAR  
+  average: 10.0,          // < $10M per WAR
   // >= $10M per WAR is Poor Value
 };
 
+// Contract efficiency thresholds
 export const EFFICIENCY_THRESHOLDS = {
   excellent: 2.0,
   good: 1.5,
   fair: 1.0,
   poor: 0.5
-};
-
-// Data source URLs
-export const DATA_SOURCES = {
-  WAR_DATA: 'https://raw.githubusercontent.com/NeilPaine/MLB-WAR-data-historical/master/war_daily_bat.txt',
-  SALARY_DATA: 'https://raw.githubusercontent.com/ianwhitestone/mlb-salaries-eda/master/data/lahman/Salaries.csv',
-  PLAYER_MAPPING: 'https://raw.githubusercontent.com/ianwhitestone/mlb-salaries-eda/master/data/lahman/People.csv'
 };
