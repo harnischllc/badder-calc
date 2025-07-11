c378c8084e371cb3cc929e9fcfda596958ff332c
harnischllc <harnischllc@gmail.com>
2025-07-11 10:05:28 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,7 @@
+// © 2024–2025 Harnisch LLC. All rights reserved.
+// Developed in partnership with BadderSports and SwingBadder.
+// Unauthorized commercial use or branding is prohibited.
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
